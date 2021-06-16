4cb2b7dc90f0010ee918dbacff7ea5985a55c1fa
wangguiren <1084424111@qq.com>
2021-06-16 22:23:03 +0800

    新增minio模块

---
+++ easy-cloud-services/easy-cloud-service-minio/src/test/java/com/easy/cloud/web/service/minio/EasyCloudServiceMinioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.cloud.web.service.minio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCloudServiceMinioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
