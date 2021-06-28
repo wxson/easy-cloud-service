8eb9384be22718db7daacc2e8809eb72b2072d72
wangguiren <1084424111@qq.com>
2021-06-28 15:35:55 +0800

    easy-cloud-web：1、调试

--- easy-cloud-register/src/main/java/com/alibaba/nacos/EasyCloudRegisterApplication.java
+++ easy-cloud-register/src/main/java/com/alibaba/nacos/EasyCloudRegisterApplication.java
@@ -1,4 +1,4 @@
-package com.easy.cloud.web.register;
+package com.alibaba.nacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EasyCloudRegisterApplication {
 
     public static void main(String[] args) {
+//        System.setProperty(ConfigConstants.TOMCAT_DIR, "nacos");
+//        System.setProperty(ConfigConstants.TOMCAT_ACCESS_LOG, "true");
+//        System.setProperty(ConfigConstants.STANDALONE_MODE, "true");
+//        System.setProperty("nacos.home", "nacos");
         SpringApplication.run(EasyCloudRegisterApplication.class, args);
     }
 
