2ee60036a9a0c99781018afbbbfd24e1c33cd097
wangguiren <1084424111@qq.com>
2021-06-28 17:44:24 +0800

    easy-cloud-web：1、新增系统分析模块

---
+++ easy-cloud-services/easy-cloud-service-charts/src/test/java/com/easy/cloud/web/service/charts/EasyCloudServiceChartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.cloud.web.service.charts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCloudServiceChartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
