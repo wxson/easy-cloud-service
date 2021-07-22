5d5123f4ef987402b7e32b027f97b2650ba04e07
wangguiren <1084424111@qq.com>
2021-07-22 15:28:09 +0800

    easy-cloud-web：1、网关服务

--- easy-cloud-gateway/src/main/java/com/easy/cloud/web/gateway/EasyCloudGatewayApplication.java
+++ easy-cloud-gateway/src/main/java/com/easy/cloud/web/gateway/EasyCloudGatewayApplication.java
@@ -2,8 +2,12 @@ package com.easy.cloud.web.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 
-@SpringBootApplication
+/**
+ * @author GR
+ */
+@SpringCloudApplication
 public class EasyCloudGatewayApplication {
 
     public static void main(String[] args) {
