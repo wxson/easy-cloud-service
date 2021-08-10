b57579e6aa4dbf1f7e72ec9ad274861b263baf02
wangguiren <1084424111@qq.com>
2021-08-10 15:16:12 +0800

    easy-cloud：1、优化

--- easy-cloud-services/easy-cloud-service-upms/easy-cloud-service-upms-biz/src/main/java/com/easy/cloud/web/service/upms/biz/EasyCloudServiceUpmsBizApplication.java
+++ easy-cloud-services/easy-cloud-service-upms/easy-cloud-service-upms-biz/src/main/java/com/easy/cloud/web/service/upms/biz/EasyCloudServiceUpmsBizApplication.java
@@ -1,8 +1,12 @@
 package com.easy.cloud.web.service.upms.biz;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author GR
+ */
 @SpringBootApplication
 public class EasyCloudServiceUpmsBizApplication {
 
