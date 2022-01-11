af34e145e9856252cc23131d17708c9ad94b64b1
wangguiren <1084424111@qq.com>
2022-01-11 17:32:47 +0800

    easy-cloud-gateway：1、优化网关服务

--- easy-cloud-gateway/src/main/java/com/easy/cloud/web/gateway/EasyCloudGatewayApplication.java
+++ easy-cloud-gateway/src/main/java/com/easy/cloud/web/gateway/EasyCloudGatewayApplication.java
@@ -3,10 +3,14 @@ package com.easy.cloud.web.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
  * @author GR
  */
+@CrossOrigin
+@EnableOpenApi
 @SpringCloudApplication
 public class EasyCloudGatewayApplication {
 
