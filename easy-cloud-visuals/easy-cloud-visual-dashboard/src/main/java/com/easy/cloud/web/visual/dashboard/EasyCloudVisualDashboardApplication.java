3e1caddc3c3e641d0523e8f73ba07018ac7f0f7f
wangguiren <1084424111@qq.com>
2022-03-12 14:28:29 +0800

    easy-cloud-service：1、新增项目可视化部署服务；// TODO

---
+++ easy-cloud-visuals/easy-cloud-visual-dashboard/src/main/java/com/easy/cloud/web/visual/dashboard/EasyCloudVisualDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.easy.cloud.web.visual.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCloudVisualDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyCloudVisualDashboardApplication.class, args);
+    }
+
+}
---
+++ easy-cloud-visuals/easy-cloud-visual-dashboard/src/test/java/com/easy/cloud/web/visual/dashboard/EasyCloudVisualDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.cloud.web.visual.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCloudVisualDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
