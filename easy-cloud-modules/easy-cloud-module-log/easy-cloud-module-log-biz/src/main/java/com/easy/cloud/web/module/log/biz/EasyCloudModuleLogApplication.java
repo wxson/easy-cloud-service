fd2f39629b0d86cf5973743613fdb954aaee5c45
GR <1084424111@qq.com>
2023-12-12 09:50:21 +0800

    fix(GR)：1、优化系统日志模块；

--- easy-cloud-modules/easy-cloud-module-log/easy-cloud-module-log-biz/src/main/java/com/easy/cloud/web/module/log/biz/EasyCloudModuleLogApplication.java
+++ easy-cloud-modules/easy-cloud-module-log/easy-cloud-module-log-biz/src/main/java/com/easy/cloud/web/module/log/biz/EasyCloudModuleLogApplication.java
@@ -3,6 +3,9 @@ package com.easy.cloud.web.module.log.biz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author GR
+ */
 @SpringBootApplication
 public class EasyCloudModuleLogApplication {
 
