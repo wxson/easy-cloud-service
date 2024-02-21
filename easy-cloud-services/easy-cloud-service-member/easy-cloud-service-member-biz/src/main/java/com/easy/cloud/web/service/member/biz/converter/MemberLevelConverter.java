0114e48be638632a1ce7614fa1aa64e91c06cd5c
GR <1084424111@qq.com>
2024-02-21 18:18:21 +0800

    fix(GR)：1、删除多余导入的文件；

--- easy-cloud-services/easy-cloud-service-member/easy-cloud-service-member-biz/src/main/java/com/easy/cloud/web/service/member/biz/converter/MemberLevelConverter.java
+++ easy-cloud-services/easy-cloud-service-member/easy-cloud-service-member-biz/src/main/java/com/easy/cloud/web/service/member/biz/converter/MemberLevelConverter.java
@@ -2,13 +2,13 @@ package com.easy.cloud.web.service.member.biz.converter;
 
 import com.easy.cloud.web.component.core.util.BeanUtils;
 import com.easy.cloud.web.service.member.api.dto.MemberLevelDTO;
-import com.easy.cloud.web.service.member.biz.domain.CompanyAuthenticationDO;
-import com.easy.cloud.web.service.member.biz.domain.MemberLevelDO;
 import com.easy.cloud.web.service.member.api.vo.MemberLevelVO;
+import com.easy.cloud.web.service.member.biz.domain.MemberLevelDO;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 /**
  * MemberLevel转换器
  *
@@ -23,7 +23,7 @@ public class MemberLevelConverter {
      * @param memberLevel 转换数据
      * @return com.easy.cloud.web.service.member.biz.domain.MemberLevelDO
      */
-    public static MemberLevelDO convertTo(MemberLevelDTO memberLevel){
+    public static MemberLevelDO convertTo(MemberLevelDTO memberLevel) {
         MemberLevelDO memberLevelDO = MemberLevelDO.builder().build();
         BeanUtils.copyProperties(memberLevel, memberLevelDO, true);
         return memberLevelDO;
@@ -35,7 +35,7 @@ public class MemberLevelConverter {
      * @param memberLevel 转换数据
      * @return com.easy.cloud.web.service.member.api.vo.MemberLevelVO
      */
-    public static MemberLevelVO convertTo(MemberLevelDO memberLevel){
+    public static MemberLevelVO convertTo(MemberLevelDO memberLevel) {
         MemberLevelVO memberLevelVO = MemberLevelVO.builder().build();
         BeanUtils.copyProperties(memberLevel, memberLevelVO, true);
         return memberLevelVO;
@@ -47,7 +47,7 @@ public class MemberLevelConverter {
      * @param memberLevels 转换数据
      * @return com.easy.cloud.web.service.member.api.vo.MemberLevelVO
      */
-    public static List<MemberLevelVO> convertTo(List<MemberLevelDO> memberLevels){
+    public static List<MemberLevelVO> convertTo(List<MemberLevelDO> memberLevels) {
         return memberLevels.stream()
                 .map(MemberLevelConverter::convertTo)
                 .collect(Collectors.toList());
@@ -59,7 +59,7 @@ public class MemberLevelConverter {
      * @param page 转换数据
      * @return com.easy.cloud.web.service.member.api.vo.MemberLevelVO
      */
-    public static Page<MemberLevelVO> convertTo(Page<MemberLevelDO> page){
+    public static Page<MemberLevelVO> convertTo(Page<MemberLevelDO> page) {
         return page.map(MemberLevelConverter::convertTo);
     }
 }
