0c677c970cc0d26fe621b1d38a20e50eee3d5dbf
GR <1084424111@qq.com>
2024-02-22 10:45:24 +0800

    fix(GR)：1、统一hutool-all依赖版本；

--- easy-cloud-components/easy-cloud-component-core/src/main/java/com/easy/cloud/web/component/core/service/IInitService.java
+++ easy-cloud-components/easy-cloud-component-core/src/main/java/com/easy/cloud/web/component/core/service/IInitService.java
@@ -2,11 +2,12 @@ package com.easy.cloud.web.component.core.service;
 
 import cn.hutool.core.io.IoUtil;
 import cn.hutool.json.JSONUtil;
+import org.springframework.util.ResourceUtils;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.util.ResourceUtils;
 
 /**
  * 初始化设置
@@ -16,48 +17,48 @@ import org.springframework.util.ResourceUtils;
  */
 public interface IInitService {
 
-  /**
-   * 初始化
-   */
-  void init();
+    /**
+     * 初始化
+     */
+    void init();
 
-  /**
-   * 初始化数据集
-   *
-   * @param path   JSON文件路径
-   * @param tClass 目标对象
-   * @param <T>    返回对象类型
-   * @return
-   */
-  default <T> List<T> initJsonToList(String path, Class<T> tClass) {
-    try {
-      // 读取JSON数据
-      File file = ResourceUtils.getFile("classpath:" + path);
-      // 读取系统菜单数据
-      String sysMenuJsonStr = IoUtil.read(IoUtil.toStream(file)).toString();
-      return JSONUtil.toList(sysMenuJsonStr, tClass);
-    } catch (FileNotFoundException fileNotFoundException) {
+    /**
+     * 初始化数据集
+     *
+     * @param path   JSON文件路径
+     * @param tClass 目标对象
+     * @param <T>    返回对象类型
+     * @return
+     */
+    default <T> List<T> initJsonToList(String path, Class<T> tClass) {
+        try {
+            // 读取JSON数据
+            File file = ResourceUtils.getFile("classpath:" + path);
+            // 读取系统菜单数据
+            String sysMenuJsonStr = IoUtil.read(IoUtil.toStream(file)).toString();
+            return JSONUtil.toList(JSONUtil.parseArray(sysMenuJsonStr), tClass);
+        } catch (FileNotFoundException fileNotFoundException) {
+        }
+        return new ArrayList<>();
     }
-    return new ArrayList<>();
-  }
 
-  /**
-   * 初始化数据对象
-   *
-   * @param path   JSON文件路径
-   * @param tClass 目标对象
-   * @param <T>    返回对象类型
-   * @return
-   */
-  default <T> T initJsonToBean(String path, Class<T> tClass) {
-    try {
-      // 读取JSON数据
-      File file = ResourceUtils.getFile("classpath:" + path);
-      // 读取系统菜单数据
-      String sysMenuJsonStr = IoUtil.read(IoUtil.toStream(file)).toString();
-      return JSONUtil.toBean(sysMenuJsonStr, tClass);
-    } catch (FileNotFoundException fileNotFoundException) {
+    /**
+     * 初始化数据对象
+     *
+     * @param path   JSON文件路径
+     * @param tClass 目标对象
+     * @param <T>    返回对象类型
+     * @return
+     */
+    default <T> T initJsonToBean(String path, Class<T> tClass) {
+        try {
+            // 读取JSON数据
+            File file = ResourceUtils.getFile("classpath:" + path);
+            // 读取系统菜单数据
+            String sysMenuJsonStr = IoUtil.read(IoUtil.toStream(file)).toString();
+            return JSONUtil.toBean(sysMenuJsonStr, tClass);
+        } catch (FileNotFoundException fileNotFoundException) {
+        }
+        return null;
     }
-    return null;
-  }
 }
