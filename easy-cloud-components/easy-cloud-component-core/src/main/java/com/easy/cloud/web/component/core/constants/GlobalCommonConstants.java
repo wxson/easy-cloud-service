23a8c4abf1a761b5b4dbbeed850ae43fc4959496
wangguiren <1084424111@qq.com>
2024-02-29 00:54:50 +0800

    fix(GR)：1、修改租户请求头字段；

--- easy-cloud-components/easy-cloud-component-core/src/main/java/com/easy/cloud/web/component/core/constants/GlobalCommonConstants.java
+++ easy-cloud-components/easy-cloud-component-core/src/main/java/com/easy/cloud/web/component/core/constants/GlobalCommonConstants.java
@@ -8,46 +8,46 @@ package com.easy.cloud.web.component.core.constants;
  */
 public class GlobalCommonConstants {
 
-  /**
-   * 租户字段
-   */
-  public static final String HEADER_TENANT = "tenant";
+    /**
+     * 租户字段
+     */
+    public static final String HEADER_TENANT = "Tenant";
 
-  /**
-   * 默认租户ID
-   */
-  public static final String DEFAULT_TENANT = "1";
+    /**
+     * 默认租户ID
+     */
+    public static final String DEFAULT_TENANT = "1";
 
-  /**
-   * 常用整型
-   */
-  public static final int SIGN_ONE = -1;
-  public static final int ZERO = 0;
-  public static final long L_ZERO = 0;
-  public static final double D_ZERO = 0.0F;
-  public static final float F_ZERO = 0.0F;
-  public static final int ONE = 1;
-  public static final int EIGHT = 8;
-  public static final int TEN = 10;
-  public static final int TWELVE = 12;
+    /**
+     * 常用整型
+     */
+    public static final int SIGN_ONE = -1;
+    public static final int ZERO = 0;
+    public static final long L_ZERO = 0;
+    public static final double D_ZERO = 0.0F;
+    public static final float F_ZERO = 0.0F;
+    public static final int ONE = 1;
+    public static final int EIGHT = 8;
+    public static final int TEN = 10;
+    public static final int TWELVE = 12;
 
-  /**
-   * 全局Redis前缀
-   */
-  public static final String REDIS_PREFIX_KEY = "easy_cloud:";
+    /**
+     * 全局Redis前缀
+     */
+    public static final String REDIS_PREFIX_KEY = "easy_cloud:";
 
-  /**
-   * 超管角色: 超级管理员获得所有权限
-   */
-  public static final String SUPER_ADMIN_ROLE = "admin";
+    /**
+     * 超管角色: 超级管理员获得所有权限
+     */
+    public static final String SUPER_ADMIN_ROLE = "admin";
 
-  /**
-   * 渠道角色编码信息
-   */
-  public static final String TENANT_ROLE = "tenant";
+    /**
+     * 渠道角色编码信息
+     */
+    public static final String TENANT_ROLE = "tenant";
 
-  /**
-   * 部门树根目录ID
-   */
-  public static final String DEPART_TREE_ROOT_ID = "0";
+    /**
+     * 部门树根目录ID
+     */
+    public static final String DEPART_TREE_ROOT_ID = "0";
 }
