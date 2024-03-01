2590bf75d4920a9892bba0d1e7d62802ae1d0c7c
GR <1084424111@qq.com>
2024-03-01 19:41:49 +0800

    fix(GR)：1、优化日志API逻辑；

--- easy-cloud-modules/easy-cloud-module-log/easy-cloud-module-log-api/src/main/java/com/easy/cloud/web/module/log/api/feign/SysLogModuleApi.java
+++ easy-cloud-modules/easy-cloud-module-log/easy-cloud-module-log-api/src/main/java/com/easy/cloud/web/module/log/api/feign/SysLogModuleApi.java
@@ -1,13 +1,11 @@
 package com.easy.cloud.web.module.log.api.feign;
 
-import com.easy.cloud.web.component.core.constants.SecurityConstants;
 import com.easy.cloud.web.component.core.constants.ServiceNameConstants;
 import com.easy.cloud.web.component.core.response.HttpResult;
 import com.easy.cloud.web.module.log.api.dto.SysLogDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 /**
  * @author GR
@@ -16,14 +14,12 @@ import org.springframework.web.bind.annotation.RequestHeader;
 @FeignClient(contextId = "sysLogModuleApi", value = ServiceNameConstants.UPMS_SERVICE)
 public interface SysLogModuleApi {
 
-  /**
-   * 保存日志
-   *
-   * @param sysLogDTO 日志实体
-   * @param from      是否内部调用
-   * @return succes、false
-   */
-  @PostMapping("/log/save")
-  HttpResult<Boolean> saveLog(@RequestBody SysLogDTO sysLogDTO,
-      @RequestHeader(SecurityConstants.ORIGIN) String from);
+    /**
+     * 保存日志
+     *
+     * @param sysLogDTO 日志实体
+     * @return succes、false
+     */
+    @PostMapping("/sys/log/save")
+    HttpResult<Boolean> saveLog(@RequestBody SysLogDTO sysLogDTO);
 }
